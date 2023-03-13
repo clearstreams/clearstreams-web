c122ddf339fc7911a11cecc464839c5e5860a114
Dhruv Jha <dhruvjha2002@gmail.com>
2023-03-13 13:27:13 +0530

    Added basic threshold card and place for the reports card

---
+++ src/components/IndexCard.svelte
@@ -0,0 +1,10 @@
+<script>
+  import IndexDataSection from "./IndexDataSection.svelte";
+</script>
+
+<div class="flex flex-col p-10 bg-base-100 rounded-md drop-shadow-md m-10">
+	<p class="font-bold pb-3 text-xl text-stone-300">Threshold Monitor</p>
+	<ul>
+    <IndexDataSection factoryName={"ITC"} indexLevel={67}/>
+  </ul>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,22 @@
   import Navbar from "../components/Navbar.svelte";
   import Footer from "../components/Footer.svelte";
   import Hero from "../components/Hero.svelte";
+  import IndexCard from "../components/IndexCard.svelte";
+  import ReportsCard from "../components/ReportsCard.svelte";
 </script>
 
 <Navbar/>
-<div>
-  <Hero />
-  <p>Dev Branch Test</p>
+
+<Hero />
+<div class="container mx-auto px-4 md:px-12">
+  <div class="flex flex-wrap -mx-1 lg:-mx-4 justify-center">
+    <IndexCard />
+    <ReportsCard />
+    <IndexCard />
+  </div>
 </div>
+
+
 <Footer />
 
 <style lang="postcss">
